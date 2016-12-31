3219022daaa1d5022f7add58f942603556a22a62
allthesignals <wilburnforce@gmail.com>
2016-12-31 13:30:37 -0500

    Fixing some tests for this branch
    These are duplicate changes because its dependent branch hasn’t been merged in yet. So it’ll probably have merge conflicts.
    
    Signed-off-by: allthesignals <wilburnforce@gmail.com>

--- mirage/config.js
+++ mirage/config.js
@@ -8,22 +8,10 @@ export default function() {
 
   this.get('features');
 
-
-  function parseFormEncodedString(requestBody) {
-    let bodyParams = {};
-    
-    requestBody.split('&').forEach((p) => {
-      let [key, value] = decodeURIComponent(p).split('=');
-      bodyParams[key] = value;
-    });
-
-    return bodyParams;
-  }
-
   this.post('token', ({ users }, request) => {
     // NOTE: the authenticator sends this as form-encoded. see: https://github.com/simplabs/ember-simple-auth/blob/master/addon/authenticators/oauth2-password-grant.js#L295.
     let parsed = JSON.parse(request.requestBody);
-    let { token, password } = parsed.user;
+    let { token } = parsed.user;
 
     let foundUser = users.findBy({
       token
--- tests/acceptance/user-logs-in-and-logs-out-successfully-test.js
+++ tests/acceptance/user-logs-in-and-logs-out-successfully-test.js
@@ -1,5 +1,5 @@
-import { describe, it, beforeEach, afterEach } from 'mocha';
-import { expect } from 'chai';
+import { describe, beforeEach, afterEach } from 'mocha';
+// import { expect } from 'chai';
 import startApp from '../helpers/start-app';
 import destroyApp from '../helpers/destroy-app';
 
@@ -20,31 +20,33 @@ describe('Acceptance | user logs in and logs out successfully', function() {
     destroyApp(application);
   });
 
-  // describe('show index', function() {
-  //   beforeEach(function() {
-  //     server.createList('user', 2);
-  //     visit('/');
-  //   });
+  describe('show index', function() {
+    beforeEach(function() {
+      server.createList('user', 2);
+      visit('/');
+    });
 
-  //   // describe('click login link', function() {
-  //   //   beforeEach(function() {
-  //   //     // click(LOGIN_LINK);
-  //   //     fillIn(EMAIL_FIELD, 'user@email.com');
-  //   //     fillIn(PASSWORD_FIELD, 'user@email.com');
-  //   //   });
+    // describe('click login link', function() {
+    //   beforeEach(function() {
+    //     click(LOGIN_LINK);
+    //     fillIn(EMAIL_FIELD, 'user@email.com');
+    //     fillIn(PASSWORD_FIELD, 'user@email.com');
+    //   });
 
-  //   //   it("shows login boxes", function() {
-  //   //     // expect(currentURL()).to.equal("/login");
-  //   //   });
+    //   it("shows login boxes", function() {
+    //     expect(currentURL()).to.equal("/login");
+    //   });
 
-  //   //   andThen(function() {
-  //   //     // click(LOGIN_BUTTON);
-  //   //     expect(currentURL()).to.equal('/');
-  //   //     expect(find(LOGOUT_LINK).text(), 'Logout');  
-  //   //   });
-      
-  //   // });
+    //   describe('logging in', function() {
+    //     beforeEach(function() {
+    //       click(LOGIN_BUTTON);
+    //     });
 
-  // });
-
+    //     it('logs the user in', function() {
+    //       expect(currentURL()).to.equal('/');
+    //       expect(find(LOGOUT_LINK).text(), 'Logout');
+    //     });
+    //   });
+    // });
+  });
 });
