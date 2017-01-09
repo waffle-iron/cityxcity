bb41e24e06de904b26622c52b23e7510a17ce2c1
allthesignals <wilburnforce@gmail.com>
2017-01-09 18:00:50 -0500

    Fix marker images
    Signed-off-by: allthesignals <wilburnforce@gmail.com>
    
    # Conflicts:
    #	config/environment.js

---
+++ app/initializers/ember-leaflet-gh-pages-image-fix.js
@@ -0,0 +1,12 @@
+import ENV from '../config/environment';
+/* global L */
+
+export function initialize(/* container, application */) {
+  L.Icon.Default.imagePath =  `${ENV.prepend || '/'}assets/images`;
+}
+
+export default {
+  name: 'leaflet-assets-cdn',
+  initialize: initialize,
+  after: 'leaflet-assets'
+};
