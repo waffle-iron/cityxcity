57e7487923065316f6e37c095f3121566333ef2c
allthesignals <wilburnforce@gmail.com>
2017-01-09 18:05:18 -0500

    Fix slash
    Signed-off-by: allthesignals <wilburnforce@gmail.com>

--- config/environment.js
+++ config/environment.js
@@ -180,7 +180,7 @@ module.exports = function(environment) {
     ENV['ember-cli-mirage'] = {
       enabled: true
     }
-    ENV.prepend = 'https://mapc.github.io/trailmap2/';
+    ENV.prepend = 'https://mapc.github.io/cityxcity/';
 
   }
 
