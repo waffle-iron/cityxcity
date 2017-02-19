763b7af4802b4a2c86b959a3467a76bd6233eebc
allthesignals <wilburnforce@gmail.com>
2017-02-18 19:41:32 -0500

    Investments and Features clustered separately

--- app/services/current-city.js
+++ app/services/current-city.js
@@ -4,12 +4,6 @@ const SOUTHWICK_LATITUDE = 42.1;
 const SOUTHWICK_LONGITUDE = -71.6;
 const DEFAULT_ZOOM = 17;
 
-const ASSET_TYPES = {
-  'privat': false,
-  'publ': false,
-  'equity': false
-}
-
 export default Ember.Service.extend({
   latitude: SOUTHWICK_LATITUDE,
   longitude: SOUTHWICK_LONGITUDE,
@@ -22,8 +16,5 @@ export default Ember.Service.extend({
       latitude,
       longitude
     });
-  },
-  applyProperty(property, value) {
-
   }
 });
