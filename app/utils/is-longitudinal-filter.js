61424330a3b530be06d053259296616c5403c377
allthesignals <wilburnforce@gmail.com>
2017-03-19 23:58:17 -0400

    more predictable filtering

--- app/utils/is-longitudinal-filter.js
+++ app/utils/is-longitudinal-filter.js
@@ -4,12 +4,19 @@ export default function isLongitudinalFilter(selectedDate, longitudinalField, mo
   let fieldDates = model.get(longitudinalField);
   let sortedDates = fieldDates.sortBy(function(o){ return new Date( o.quarter ) });
   let state = false;
-  sortedDates.forEach((el, index) => {
-    let futureDate = sortedDates[index + 1];
-    if (!futureDate) return;
-    if ((truthState == el.status) && (new Date(selectedDate) < sortedDates[index + 1].quarter)) {
-      state = true;
-    }
-  });
+
+  // is selected date within the range? 
+  if ((new Date(selectedDate) > sortedDates[0].quarter) && (new Date(selectedDate) < sortedDates[sortedDates.length-1].quarter) ) {
+
+    // what is the status of the selectedDate?
+    sortedDates.forEach((el, index) => {
+      let futureDate = sortedDates[index + 1];
+      if (!futureDate) return;
+      if ((truthState == el.status) && (new Date(selectedDate) < sortedDates[index + 1].quarter)) {
+        state = true;
+      }
+      // console.log(model.id, ": ", el.quarter, el.status, state);  
+    });
+  }
   return state;
 }
