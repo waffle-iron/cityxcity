a9bfa33bc8dd744fa101fa5c8f909d888dab92e6
allthesignals <wilburnforce@gmail.com>
2017-03-20 22:44:14 -0400

    Rework of the islongitudinal util

--- app/utils/is-longitudinal-filter.js
+++ app/utils/is-longitudinal-filter.js
@@ -4,19 +4,30 @@ export default function isLongitudinalFilter(selectedDate, longitudinalField, mo
   let fieldDates = model.get(longitudinalField);
   let sortedDates = fieldDates.sortBy(function(o){ return new Date( o.quarter ) });
   let state = false;
+  let min = sortedDates[0].quarter;
+  let max = sortedDates[sortedDates.length - 1].quarter;
 
-  // is selected date within the range? 
-  if ((new Date(selectedDate) > sortedDates[0].quarter) && (new Date(selectedDate) < sortedDates[sortedDates.length-1].quarter) ) {
+  // what is the status of the selectedDate?
+  sortedDates.forEach((el, index) => {
+    let subsequentDate = sortedDates[index + 1];
 
-    // what is the status of the selectedDate?
-    sortedDates.forEach((el, index) => {
-      let futureDate = sortedDates[index + 1];
-      if (!futureDate) return;
-      if ((truthState == el.status) && (new Date(selectedDate) < sortedDates[index + 1].quarter)) {
+    // is there a subsequent date or is this the last?
+    if (subsequentDate) {
+      let parsedSubsequentDate = new Date(selectedDate);
+      
+      // is the selected date greater than the current element date and less than the subsequent element date? 
+      if ( (new Date(selectedDate) > el.quarter) && (new Date(selectedDate) < new Date(subsequentDate.quarter)) ) {
+        if (el.status == truthState) {
+          state = true;
+        }
+      }
+    } else {
+      // it's the last in the array anyway, check its truth state
+      if (el.status == truthState) {
         state = true;
       }
-      // console.log(model.id, ": ", el.quarter, el.status, state);  
-    });
-  }
+    }
+  });
+
   return state;
 }
