28fec41d4692f937cad2566aa9397d5f149c5427
allthesignals <wilburnforce@gmail.com>
2017-03-27 05:04:11 -0400

    exclude finger prints

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -12,7 +12,9 @@ module.exports = function(defaults) {
         'images/layers.png',
         'images/marker-icon-2x.png',
         'images/marker-icon.png',
-        'images/marker-shadow.png'
+        'images/marker-shadow.png',
+        'images/icons',
+        'images/icons/*'
       ]
     }
   });
